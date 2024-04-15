bce5ba80693233a7aa28262e42564100321005e1
keyser-fr <keyser_fr@hotmail.com>
2024-04-15 12:12:08 +0200

    Handle +30 days expiration token

--- scripts/check_gitlab_token.sh
+++ scripts/check_gitlab_token.sh
@@ -9,11 +9,12 @@ GITLAB_API_VERSION="v4"
 GITLAB_API_URL="${GITLAB_URL}/${GITLAB_API}/${GITLAB_API_VERSION}"
 GITLAB_TOKEN_FILE=${HOME}/.git-credentials
 GITLAB_TOKEN=$(grep -Ew "gitlab_token" ${GITLAB_TOKEN_FILE} | awk '{print $NF}')
-GITLAB_TOKEN_THRESHOLD_ALERT=7200 # 7200 (2 day) & 604800 (7 days)
+GITLAB_TOKEN_THRESHOLD_ALERT=7200 # 7200 (2 days) & 604800 (7 days)
 GITLAB_ACCESS_INFO=$(curl --silent --request GET --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" "${GITLAB_API_URL}/personal_access_tokens?revoked=false")
 GITLAB_TOKEN_EXPIRED_AT=$(jq -r '.[].expires_at' <<< ${GITLAB_ACCESS_INFO})
 GITLAB_TOKEN_EXPIRED_AT_TS=$(date --date="${GITLAB_TOKEN_EXPIRED_AT}" +"%s")
 NOW_DATE_TS=$(date --date="now" +"%s")
+DATE_30_DAYS_EXPIRATION=$(date --date="now +30 days" "+%Y-%m-%d")
 DEST_DIR="${HOME}/rescue/sql/sql.free.fr"
 SUP_FILE=${DEST_DIR}/gitlab_token.expired
 
@@ -28,7 +29,7 @@ if (( $(( ${GITLAB_TOKEN_EXPIRED_AT_TS} - ${NOW_DATE_TS} )) <= 0 )); then
 elif (( $(( ${GITLAB_TOKEN_EXPIRED_AT_TS} - ${NOW_DATE_TS} )) <= ${GITLAB_TOKEN_THRESHOLD_ALERT} )); then
     # Renew token (rotate)
     GITLAB_TOKEN_ID=$(jq -r '.[].id' <<< ${GITLAB_ACCESS_INFO})
-    GITLAB_TOKEN_ROTATE=$(curl --silent --request POST --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}"  "${GITLAB_API_URL}/personal_access_tokens/${GITLAB_TOKEN_ID}/rotate")
+    GITLAB_TOKEN_ROTATE=$(curl --silent --request POST --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}"  "${GITLAB_API_URL}/personal_access_tokens/${GITLAB_TOKEN_ID}/rotate?expires_at=${DATE_30_DAYS_EXPIRATION}")
     GITLAB_TOKEN_RENEW=$(jq -r '.token' <<< ${GITLAB_TOKEN_ROTATE})
     chmod 600 ${GITLAB_TOKEN_FILE}
     echo "gitlab_token = ${GITLAB_TOKEN_RENEW}" > ${GITLAB_TOKEN_FILE}
@@ -36,7 +37,7 @@ elif (( $(( ${GITLAB_TOKEN_EXPIRED_AT_TS} - ${NOW_DATE_TS} )) <= ${GITLAB_TOKEN_
 else
     # Remove file for supervision
     if [[ -f ${SUP_FILE} ]]; then
-	rm ${SUP_FILE}
+	rm -f ${SUP_FILE}
     fi
 fi
 
