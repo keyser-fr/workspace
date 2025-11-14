11fe278218b3c839e2b59233f36972bb60d64561
keyser-fr <keyser_fr@hotmail.com>
2025-11-14 10:55:10 +0100

    Update script synchro dedibackup

--- scripts/sql_dedibackup.sh
+++ scripts/sql_dedibackup.sh
@@ -4,8 +4,8 @@
 # set -x # debug mode => equivalent for bash -x command
 
 DATE_NOW=$(date +"%F_%T")
-SOURCE_DIR="${HOME}/rescue/sql/sql.free.fr"
-DEST_DIR="${HOME}/dedibackup/mysql/"
+SOURCE_DIR=${SOURCE_DIR:-"${HOME}/rescue/sql/sql.free.fr"}
+DEST_DIR=${DEST_DIR:-"${HOME}/dedibackup/backup/sql/"}
 
 # $(which cp) -r ${SOURCE_DIR} ${DEST_DIR}
 $(which rsync) -avz --human-readable --delete ${SOURCE_DIR} ${DEST_DIR} #--dry-run
