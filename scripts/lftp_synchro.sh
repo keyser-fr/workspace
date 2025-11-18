b6eec2ddef0a3eb06e7e86b31aa2a8d1672d4239
keyser-fr <keyser_fr@hotmail.com>
2025-11-18 09:40:09 +0100

    Fix logfile

--- scripts/lftp_synchro.sh
+++ scripts/lftp_synchro.sh
@@ -6,10 +6,12 @@
 DATE_NOW=$(date +"%F_%T")
 SOURCE_DIR=${SOURCE_DIR:-"${HOME}/dedibackup/"}
 DEST_DIR=${DEST_DIR:-"/dedibackup"}
-# LOG_DIR=${LOG_DIR:-${HOME}/.local/share/lftp/transfer_log} # default directory
-LOG_DIR=${LOG_DIR:-${HOME}/var/log"}
+# LOG_DIR=${LOG_DIR:-"${HOME}/.local/share/lftp"} # default directory
+# LOGFILE=${LOGFILE:-"${LOG_DIR}/transfer_log"} # default file
+LOG_DIR=${LOG_DIR:-"${HOME}/var/log"}
+LOGFILE=${LOGFILE:-"${LOG_DIR}/dedibackup.log"}
 
-$(which lftp) ftp://auto:@dedibackup-dc3.online.net -e "set log:file/xfer ${LOG_DIR}; mirror -e -R ${SOURCE_DIR} ${DEST_DIR}; quit"
+$(which lftp) ftp://auto:@dedibackup-dc3.online.net -e "set log:file/xfer ${LOGFILE}; mirror -e -R ${SOURCE_DIR} ${DEST_DIR}; quit"
 RETVAL=${?}
 
 if [[ ${RETVAL} == 0 ]]; then
